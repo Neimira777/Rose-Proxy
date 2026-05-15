0bef88d36f3b2b2149caf8e4e35c8238307544b9
Neimira777 <artistic14allure@gmail.com>
2026-05-15 18:57:28 -0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -7,6 +7,9 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
+  if (req.method === 'GET') {
+    return res.status(200).json({ status: 'ok' });
+  }
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
