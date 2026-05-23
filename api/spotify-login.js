a941c31595d7174bc685c8c1e00103eda7d4aacb
Neimira777 <artistic14allure@gmail.com>
2026-05-23 15:31:37 -0400

    Update spotify-login.js

--- api/spotify-login.js
+++ api/spotify-login.js
@@ -1,1 +1,25 @@
+export default function handler(req, res) {
+  const { patientId } = req.query;
 
+  if (!patientId) {
+    return res.status(400).json({ error: 'Missing patientId' });
+  }
+
+  const scope = [
+    'streaming',
+    'user-read-email',
+    'user-read-private',
+    'user-modify-playback-state',
+    'user-read-playback-state'
+  ].join(' ');
+
+  const params = new URLSearchParams({
+    response_type: 'code',
+    client_id: process.env.SPOTIFY_CLIENT_ID,
+    scope,
+    redirect_uri: 'https://rose-proxy.vercel.app/api/spotify-callback',
+    state: patientId
+  });
+
+  return res.redirect(`https://accounts.spotify.com/authorize?${params.toString()}`);
+}
