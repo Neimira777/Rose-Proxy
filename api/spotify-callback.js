064297ecdcc4c22376c88eccee8289416bf8babf
Neimira777 <artistic14allure@gmail.com>
2026-05-23 17:24:35 -0400

    Update spotify-callback.js

--- api/spotify-callback.js
+++ api/spotify-callback.js
@@ -25,8 +25,7 @@ export default async function handler(req, res) {
   if (!tokenData.access_token) {
     return res.status(500).send('Token error: ' + JSON.stringify(tokenData));
   }
-
-  const airtableUrl = 'https://api.airtable.com/v0/' + process.env.AIRTABLE_BASE_ID + '/' + process.env.AIRTABLE_TABLE_ID + '/' + patientId;
+const airtableUrl = 'https://api.airtable.com/v0/' + process.env.AIRTABLE_BASE_ID + '/tblWZWMZNWfpbVVRX/' + patientId;
   console.log('Saving to Airtable:', airtableUrl);
 
   const airtableRes = await fetch(airtableUrl, {
