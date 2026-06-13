4fae71864020a43bb77e7b357c6d71ce5ea4a19f
Neimira777 <artistic14allure@gmail.com>
2026-06-13 16:03:56 -0400

    Add DELETE method to music-queue to clear on session start

--- api/music-queue.js
+++ api/music-queue.js
@@ -32,5 +32,11 @@ export default async function handler(req, res) {
     return res.status(200).json({ query });
   }
 
+  if (req.method === 'DELETE') {
+    const { patientId } = req.query || {};
+    if (patientId) delete musicQueue[patientId];
+    return res.status(200).json({ ok: true });
+  }
+
   return res.status(405).json({ error: 'Method not allowed' });
 }
